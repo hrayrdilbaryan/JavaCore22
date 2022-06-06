04615d16d0675f1ef63c4d7e734cf653bbcdeffd
Hrayr Dilbaryan <hrair.dilbaryan@mail.ru>
2022-06-06 19:37:06 +0400

    OverloadTest

---
+++ src/classwork/lesson12/OverloadTest.java
@@ -0,0 +1,17 @@
+package classwork.lesson12;
+
+public class OverloadTest {
+
+    void test() {
+        System.out.println(" I am test()");
+    }
+
+    void test(int a) {
+        System.out.println("I am test(int a)");
+    }
+
+    void test(String a) {
+        System.out.println(" I am test(String a)");
+    }
+
+}
---
+++ src/classwork/lesson12/OverloadTestDemo.java
@@ -0,0 +1,10 @@
+package classwork.lesson12;
+
+public class OverloadTestDemo {
+    public static void main(String[] args) {
+
+        OverloadTest overloadTest = new OverloadTest();
+        overloadTest.test(4);
+
+    }
+}
